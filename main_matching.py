7bbfb8e6cad37b957a2df6ab7a6c95e3c9e3c4f1
Ruan Montelo <ruanmontelo@gmail.com>
2021-01-07 01:36:53 -0300

    Adding main_matching for pattern matching between frames and adding comments and canny image to main_using_fast.py file

---
+++ main_matching.py
@@ -0,0 +1,62 @@
+"""
+    WORKING MATCHER USING ORB + KNN MATCHING DETECTION
+"""
+
+import cv2
+import numpy as np
+
+capture = cv2.VideoCapture('driving.mp4')
+last_frame = None
+
+if not capture.isOpened():
+    print('Error opening video.')
+
+while capture.isOpened():
+    ret, frame = capture.read()
+    if ret:
+        if last_frame is not None:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray_from_last_frame = cv2.cvtColor(last_frame, cv2.COLOR_BGR2GRAY)
+
+            # Initiate ORB detector
+            orb = cv2.ORB_create(100)
+
+            blurred_gray = cv2.GaussianBlur(gray, (5, 5), 0)
+            blurred_gray_last_frame = cv2.GaussianBlur(gray_from_last_frame, (5, 5), 0)
+
+            # find the key points with ORB
+            # kp, des = orb.detectAndCompute(gray, None)
+            # kp2, des2 = orb.detectAndCompute(gray_from_last_frame, None)
+            kp, des = orb.detectAndCompute(blurred_gray, None)
+            kp2, des2 = orb.detectAndCompute(blurred_gray_last_frame, None)
+
+            bf = cv2.BFMatcher()
+            if des is not None and des2 is not None and len(des) == len(des2):
+                matches = bf.knnMatch(des, des2, k=2)
+
+                for keypoint in kp:
+                    for keypoint2 in kp2:
+                        if keypoint == keypoint2:
+                            print('MATCHED')
+
+                good = []
+                for m, n in matches:
+                    if m.distance < 0.3 * n.distance:
+                        good.append([m])
+
+                draw_params = dict(matchColor=(0, 255, 0),
+                                   flags=2)
+
+                img = cv2.drawMatchesKnn(gray, kp, gray_from_last_frame, kp2, good, outImg=None, **draw_params)
+
+                cv2.imshow('Seal', img)
+
+            if cv2.waitKey(20) & 0xFF == ord('q'):
+                break
+
+        last_frame = frame
+
+    continue
+
+capture.release()
+cv2.destroyAllWindows()
--- main_using_fast.py
+++ main_using_fast.py
@@ -1,24 +1,53 @@
 import cv2
+import numpy as np
+
+# TODO LIST:
+# Create a mask image
+# Detect the lines to draw
+# Get mask image with drawn lines
+# Draw lines
+# Sum the binary content of both images (masked and grayscale) into one image to display the drawn lanes + keypoints.
+
+# The last item might help identify if an object is closer than it should be to the car.
+# TODO FUTURE RUAN: Check if the keypoints are close or inside the "triangle" that composes a lane. That should do it.
+# TODO FUTURE RUAN: Check if the keypoints are a bit upper to the lane; They might represent a close object at the end.
+# TODO FUTURE RUAN: Computational Lane Detection -> https://www.youtube.com/watch?v=eLTLtUVuuy4&ab_channel=ProgrammingKnowledge
 
 # capture = cv2.VideoCapture('day_in_the_life.mp4')
-# capture = cv2.VideoCapture('driving.mp4')
-capture = cv2.VideoCapture('walking_on_the_street.mp4')
+capture = cv2.VideoCapture('driving.mp4')
+# capture = cv2.VideoCapture('walking_on_the_street.mp4')
 
 if not capture.isOpened():
     print('Error while opening video.')
 
+COMMON_THRESHOLD = 100
+
 
 class FastDetector:
     def __init__(self, current_frame, threshold: int):
-        self.converted_img_from_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2RGB)
+        self.converted_img_from_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
         self.fast_instance = cv2.FastFeatureDetector_create(threshold)
+        self.blurred = cv2.GaussianBlur(self.converted_img_from_frame, (5, 5), 0)
+        self.canny = cv2.Canny(self.blurred, 50, 150)
+        self.lines = None
         self.kp = []
 
     def set_kp_from_converted_img(self):
         self.kp = self.fast_instance.detect(self.converted_img_from_frame, None)
 
-    def get_image_with_keypoints(self, color: tuple):
-        return cv2.drawKeypoints(self.converted_img_from_frame, self.kp, None, color=color,
+    def draw_lines(self):
+        # TODO: pass in the cropped_image, not canny
+        self.lines = cv2.HoughLinesP(self.canny, 2, np.pi/180,
+                                     COMMON_THRESHOLD,
+                                     np.array([]),
+                                     minLineLength=40,
+                                     maxLineGap=5)
+
+    def get_image_with_keypoints(self, color: tuple, run_canny: bool):
+        if run_canny:
+            return cv2.drawKeypoints(self.canny, self.kp, None, color, cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+        return cv2.drawKeypoints(self.blurred, self.kp, None, color=color,
                                  flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 
@@ -28,11 +57,11 @@ while capture.isOpened():
     video_height: float = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
     middle = (video_height // 2, video_width // 2)
     if captured:
-        fd = FastDetector(frame, threshold=100)
+        fd = FastDetector(frame, threshold=COMMON_THRESHOLD)
 
         fd.set_kp_from_converted_img()
 
-        img_with_keypoints = fd.get_image_with_keypoints((0, 255, 0))
+        img_with_keypoints = fd.get_image_with_keypoints((0, 255, 0), run_canny=True)
 
         for keypoint in fd.kp:
             if abs(keypoint.pt[0] - middle[0]) <= 150 or abs(keypoint.pt[1] - middle[1]) <= 150:
@@ -40,7 +69,7 @@ while capture.isOpened():
 
         cv2.imshow('Seal', img_with_keypoints)
 
-        if cv2.waitKey(30) & 0xFF == ord('q'):
+        if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
 capture.release()
